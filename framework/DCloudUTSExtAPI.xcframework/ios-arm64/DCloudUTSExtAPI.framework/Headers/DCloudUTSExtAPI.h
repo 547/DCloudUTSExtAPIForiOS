b0e3391fecb43701b87e598e2ad76bd39bddd17e
王琦 <A80741@gree.com.cn>
2025-04-08 15:12:24 +0800

    添加 根据uni-app x iOS原生SDK正式版4.57.2025032507编译的xcframework

---
+++ framework/DCloudUTSExtAPI.xcframework/ios-arm64/DCloudUTSExtAPI.framework/Headers/DCloudUTSExtAPI.h
@@ -0,0 +1,21 @@
+//
+//  DCloudUTSExtAPI.h
+//  DCloudUTSExtAPI
+//
+//  Created by wangzhitong on 2024/6/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DCloudUTSExtAPI.
+FOUNDATION_EXPORT double DCloudUTSExtAPIVersionNumber;
+
+//! Project version string for DCloudUTSExtAPI.
+FOUNDATION_EXPORT const unsigned char DCloudUTSExtAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DCloudUTSExtAPI/PublicHeader.h>
+#import <DCloudUTSExtAPI/WechatAuthSDK.h>
+#import <DCloudUTSExtAPI/WXApi.h>
+#import <DCloudUTSExtAPI/WXApiObject.h>
+
+
